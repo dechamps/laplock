246e49ca806dc20428283a59135115532058ba07
Etienne Dechamps <etienne@edechamps.fr>
2016-12-25 15:17:28 +0100

    Add debug logging functionality.

--- laplock.cpp
+++ laplock.cpp
@@ -15,25 +15,57 @@
  */
 
 #include <cstdlib>
+#include <fstream>
+#include <memory>
 #include <sstream>
 
 #include <windows.h>
 
+class LogLine {
+public:
+	~LogLine() {
+		if (!logfile) return;
+		*logfile << std::endl;
+		logfile->flush();
+	}
+
+	template <typename T> LogLine& operator<<(T value) {
+		if (!logfile) return *this;
+		*logfile << value;
+		return *this;
+	}
+
+	static std::unique_ptr<std::wofstream> logfile;
+};
+
+std::unique_ptr<std::wofstream> LogLine::logfile;
+
+std::wostream& operator<<(std::wostream& stream, const GUID& guid) {
+	wchar_t str[128] = { 0 };
+	StringFromGUID2(guid, str, sizeof(str));
+	stream << str;
+	return stream;
+}
+
 static void systemError(wchar_t* what)
 {
+	const DWORD error = GetLastError();
+	LogLine() << "Error " << error << " during: " << what;
 	static wchar_t buffer[1024];
 	const wchar_t* errorMessage = buffer;
 
 	if (!FormatMessage(
 		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 		0,
-		GetLastError(),
+		error,
 		0,
 		buffer,
 		sizeof(buffer),
 		0
 	))
 		errorMessage = L"(cannot format error message)";
+	else
+		LogLine() << "System error message: " << errorMessage;
 
 	std::wstringstream message;
 	message << L"A system error occured within laplock." << std::endl;
@@ -46,25 +78,42 @@ static void systemError(wchar_t* what)
 
 static LRESULT CALLBACK windowProcedure(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	if (uMsg != WM_POWERBROADCAST || wParam != PBT_POWERSETTINGCHANGE || GetSystemMetrics(SM_REMOTESESSION))
+	if (uMsg != WM_POWERBROADCAST || wParam != PBT_POWERSETTINGCHANGE) {
+		LogLine() << "Window received irrelevant message";
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+
+	if (GetSystemMetrics(SM_REMOTESESSION)) {
+		LogLine() << "Ignoring window message because session is currently remote";
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 
 	const POWERBROADCAST_SETTING* setting = reinterpret_cast<const POWERBROADCAST_SETTING*>(lParam);
-	if (setting->PowerSetting != GUID_MONITOR_POWER_ON && setting->PowerSetting != GUID_LIDSWITCH_STATE_CHANGE)
+	LogLine() << "Received POWERBROADCAST_SETTING " << setting->PowerSetting;
+	if (setting->PowerSetting != GUID_MONITOR_POWER_ON && setting->PowerSetting != GUID_LIDSWITCH_STATE_CHANGE) {
+		LogLine() << "Received irrelevant POWERBROADCAST_SETTING";
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 
 	const DWORD* state = reinterpret_cast<const DWORD*>(&setting->Data);
-	if (*state != 0)
+	LogLine() << "POWERBROADCAST_SETTING state: " << *state;
+	if (*state != 0) {
+		LogLine() << "Irrelevant POWERBROADCAST_SETTING state";
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 
+	LogLine() << "Locking";
 	if (LockWorkStation() == 0)
 		systemError(L"locking workstation");
+	else
+		LogLine() << "Locked";
 
 	return 0;
 }
 
 static void registerWindowClass(HINSTANCE instance)
 {
+	LogLine() << "Registering window class";
 	WNDCLASSEX windowClass = { 0 };
 
 	windowClass.cbSize = sizeof(WNDCLASSEX);
@@ -78,6 +127,7 @@ static void registerWindowClass(HINSTANCE instance)
 
 static HWND createWindow(HINSTANCE instance)
 {
+	LogLine() << "Creating window";
 	HWND hWnd = CreateWindow(
 		L"laplock",
 		NULL,
@@ -100,8 +150,10 @@ static HWND createWindow(HINSTANCE instance)
 
 static void registerNotification(HWND window)
 {
+	LogLine() << "Registering GUID_MONITOR_POWER_ON (GUID: " << GUID_MONITOR_POWER_ON << ")";
 	if (!RegisterPowerSettingNotification(window, &GUID_MONITOR_POWER_ON, DEVICE_NOTIFY_WINDOW_HANDLE))
 		systemError(L"cannot register GUID_MONITOR_POWER_ON power setting notification");
+	LogLine() << "Registering GUID_LIDSWITCH_STATE_CHANGE (GUID: " << GUID_LIDSWITCH_STATE_CHANGE << ")";
 	if (!RegisterPowerSettingNotification(window, &GUID_LIDSWITCH_STATE_CHANGE, DEVICE_NOTIFY_WINDOW_HANDLE))
 		systemError(L"cannot register GUID_LIDSWITCH_STATE_CHANGE power setting notification");
 }
@@ -110,23 +162,31 @@ static WPARAM messageLoop()
 {
 	for (;;)
 	{
+		LogLine() << "Awaiting next window message";
 		MSG message;
 		BOOL result = GetMessage(&message, NULL, 0, 0);
 		if (result == -1)
 			systemError(L"getting window message");
 		if (result == 0)
 			return message.wParam;
+		LogLine() << "Dispatching message";
 		DispatchMessage(&message);
 	}
 }
 
-int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR commandLine, int)
 {
+	if (*commandLine != 0)
+		LogLine::logfile.reset(new std::wofstream(commandLine, std::ios_base::app));
+	LogLine() << "laplock initializing";
+
 	registerWindowClass(instance);
 
 	HWND window = createWindow(instance);
 
 	registerNotification(window);
 
-	return messageLoop();
+	WPARAM result = messageLoop();
+	LogLine() << "laplock terminating";
+	return result;
 }
